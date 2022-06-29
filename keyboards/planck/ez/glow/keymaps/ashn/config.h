79f439fe4c2433b83d93f5e97adf3d1220a022d0
Ash <ngashcraft@gmail.com>
2022-06-28 17:00:54 -0700

    Add configuration for personal Planch layout.

---
+++ keyboards/planck/ez/glow/keymaps/ashn/config.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define IGNORE_MOD_TAP_INTERRUPT
+#define FIRMWARE_VERSION u8"njLQD/G7m3j"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define RGB_MATRIX_STARTUP_SPD 60
